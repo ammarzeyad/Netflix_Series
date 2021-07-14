bb9744d9146877930d80120832e772a4803a1059
ammarzeyad <>
2021-07-14 12:57:11 +0000

    add a app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+var name = prompt("Heyy What Is Your name ?")
+
+alert("Heey, wlecome To My Modest Website " + name)
+
+var Favorit_Series = prompt("What is you FavoritSeries ?")
+
+if(Favori_tSeries == "Peaky Blinders"){
+document.write(
+  "<div>" + "<h3>" + Favorit_Series+"</h3>"
+  +
+  "https://upload.wikimedia.org/wikipedia/en/e/e8/Peaky_Blinders_titlecard.jpg" 
+else if(Favorit_Series == "The Queen's Gambit"){
+document.write(
+  "<div>" + "<h3>" + Favorit_Series+"</h3>"
+  +
+  "https://comicyears.com/wp-content/uploads/2020/10/The-Queens-Gambit-Review-Netflix-tv-series.jpg"
+}
+
+else if(Favorit_Movies =="God Father"){
+document.write(
+  "<div>" + "<h3>" + Favorit_Movies+"</h3>"
++ "<img  src='https://wallpaperaccess.com/full/1107715.jpg" + "</div>")
+}
+else if(Favorit_Movies =="God Father"){
+document.write(
+  "<div>" + "<h3>" + Favorit_Movies+"</h3>"
++ "<img  src='https://wallpaperaccess.com/full/1107715.jpg" + "</div>")
+
+else if(Favorit_Movies =="The Social Network’ "){
+document.write(
+  "<div>" + "<h3>" + Favorit_Movies+"</h3>"
++ "<img  src='https://static01.nyt.com/images/2020/10/05/arts/05social-network01/05social-network01-superJumbo-v3.jpg?quality=90&auto=webp" + "</div>")
+
+else{
+  alert('we dont have this Show')
+}
+
